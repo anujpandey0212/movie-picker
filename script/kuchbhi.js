fac0ec2459e054835d7cf00cc33f1321c2f62c2b
anujpandey0212 <MS20BTECH11003@iith.ac.in>
2021-08-10 02:56:11 +0530

    re used

--- script/kuchbhi.js
+++ script/kuchbhi.js
@@ -2,58 +2,58 @@ const api= '&api_key=4bc21d3df69b28e4577bcb4e747bd00e';
 const base_url='https://api.themoviedb.org/3/';
 const upper_genres='discover/movie?with_genres=';
 const lower_generes='&sort_by=popularity.desc&vote_count.gte=10';
-const images_base_url='https://image.tmdb.org/t/p/w500';
+const img_base_url='https://image.tmdb.org/t/p/w500';
 const lang='&language=hi';
 const main = document.getElementById('main');
 
   function majekro()
   {
     var favoritemovie = sessionStorage.getItem("favoriteMovie");
-    if(favoritemovie=="images1"||favoritemovie=="link1")
+    if(favoritemovie=="img1"||favoritemovie=="link1")
     {
       var id="9648";
     }
-    if(favoritemovie=="images2"||favoritemovie=="link2")
+    if(favoritemovie=="img2"||favoritemovie=="link2")
     {
       var id="18";
     }
-    if(favoritemovie=="images3"||favoritemovie=="link3")
+    if(favoritemovie=="img3"||favoritemovie=="link3")
     {
       var id="53";
     }
-    if(favoritemovie=="images4"||favoritemovie=="link4")
+    if(favoritemovie=="img4"||favoritemovie=="link4")
     {
       var id="10749";
     }
-    if(favoritemovie=="images5"||favoritemovie=="link5")
+    if(favoritemovie=="img5"||favoritemovie=="link5")
     {
       var id="27";
     }
-    if(favoritemovie=="images6"||favoritemovie=="link6")
+    if(favoritemovie=="img6"||favoritemovie=="link6")
     {
       var id="99";
     }
-    if(favoritemovie=="images7"||favoritemovie=="link7")
+    if(favoritemovie=="img7"||favoritemovie=="link7")
     {
       var id="878";
     }
-    if(favoritemovie=="images8"||favoritemovie=="link8")
+    if(favoritemovie=="img8"||favoritemovie=="link8")
     {
       var id="28";
     }
-    if(favoritemovie=="images9"||favoritemovie=="link9")
+    if(favoritemovie=="img9"||favoritemovie=="link9")
     {
       var id="35";
     }
-    if(favoritemovie=="images10"||favoritemovie=="link10")
+    if(favoritemovie=="img10"||favoritemovie=="link10")
     {
       var id="14";
     }
-    if(favoritemovie=="images11"||favoritemovie=="link11")
+    if(favoritemovie=="img11"||favoritemovie=="link11")
     {
       var id="12";
     }
-    if(favoritemovie=="images12"||favoritemovie=="link12")
+    if(favoritemovie=="img12"||favoritemovie=="link12")
     {
       var id="10751";
     }
@@ -76,7 +76,7 @@ const main = document.getElementById('main');
              const movieE1 = document.createElement('div');
              movieE1.classList.add('movie');
              movieE1.innerHTML=`
-             <images id="goku" src="${images_base_url+poster_path}" alt="${title}">
+             <img id="goku" src="${img_base_url+poster_path}" alt="${title}">
              <p id="title">Title : ${title}</p>
              <p id="rating">Rating : ${vote_average}</p>
              <div id="overview"><h3>Overview</h3>${overview}</div>
